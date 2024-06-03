c97989a0d4cc3015416afdd07418c73620ca53f7
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-06-03 15:21:31 +0200

    getting product via sku by customer / admin

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -7,10 +7,11 @@ const productController = {};
 productController.createProduct = async (req, res) => {
   try {
     const userId = req.userId;
-    const user = await User.findById(userId);
-    if (user.role === 'customer') {
+    const isAdmin = await isUserAdmin(userId, res);
+    if (!isAdmin) {
       return res.status(403).json({ message: 'Access denied' });
     }
+
     const { sku, name, description, price, image, stock, category, status, isDeleted } = req.body;
     const exProduct = await Product.findOne({ sku: sku });
     if (exProduct) {
@@ -26,4 +27,34 @@ productController.createProduct = async (req, res) => {
   }
 };
 
+productController.getProductBySKU = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const isAdmin = await isUserAdmin(userId, res);
+
+    const sku = req.params.sku;
+    const product = await Product.findOne({ sku: sku });
+    if (!product) {
+      return res.status(404).send('product not found or unknown sku!');
+    }
+    if (!isAdmin) {
+      if (product.isDeleted || product.status === 'inactive') {
+        return res.status(400).json({ message: 'this product is deleted or inactive' });
+      }
+      res.status(200).json(product);
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error by getting product', err });
+  }
+};
+
+const isUserAdmin = async (userId, res) => {
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(404).json({ message: 'user not found' });
+  }
+  return user.role !== 'customer';
+};
 module.exports = productController;
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { authenticate } = require('../controller/auth.controller');
-const { createProduct } = require('../controller/product.controller');
+const { createProduct, getProductBySKU } = require('../controller/product.controller');
 
 const router = express.Router();
 
 router.post('/', authenticate, createProduct);
-// router.get('/:productId', authenticate, getProductById);
+router.get('/:sku', authenticate, getProductBySKU);
 // router.get('/', authenticate, getAllProducts);
 
 module.exports = router;
