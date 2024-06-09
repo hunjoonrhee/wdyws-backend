35030e3c90e36b46d0cf973d5e9be875b0e2e402
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-06-10 00:55:19 +0200

    get order list

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -8,6 +8,8 @@ const mongoose = require('mongoose');
 const productConrtoller = require('./product.controller');
 const RandomStringGenerator = require('../utils/randomStringGenerator');
 
+const PAGE_SIZE = 4;
+
 orderController.createOrder = async (req, res) => {
   try {
     if (req.isGuest) {
@@ -39,6 +41,70 @@ orderController.createOrder = async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Error by creating an order', err });
+  }
+};
+
+orderController.getOrderList = async (req, res) => {
+  try {
+    const { page, orderNum } = req.query;
+    const cond = orderNum ? { orderNum: { $regex: orderNum, $options: 'i' } } : {};
+    let query = Order.find(cond)
+      .populate({
+        path: 'orderItems',
+        populate: {
+          path: 'productId',
+          model: 'Product',
+        },
+      })
+      .populate({
+        path: 'userId',
+        model: 'User',
+      });
+    let response = { status: 'success' };
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      const totalItemNum = await Order.find(cond).count();
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+      response.pageSize = PAGE_SIZE;
+    }
+    const order = await query.exec();
+    response.data = order;
+    if (!order) {
+      return res.status(404).json({ message: 'order not found!' });
+    }
+    if (req.isAdmin) {
+      return res.status(200).json({ response });
+    } else {
+      return res.status(403).json({ message: 'permission denied!' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error by getting all order lists', err });
+  }
+};
+orderController.getMyOrderList = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'user not found!' });
+    }
+    const order = await Order.findOne({ userId: userId }).populate({
+      path: 'orderItems',
+      populate: {
+        path: 'productId',
+        model: 'Product',
+      },
+    });
+    if (!order) {
+      return res.status(404).json({ message: 'order not found!' });
+    }
+    const myOrderList = order.orderItems;
+    return res.status(200).json({ status: 'success', myOrderList });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error by getting user order list', err });
   }
 };
 module.exports = orderController;
--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { authenticate } = require('../controller/auth.controller');
-const { createOrder } = require('../controller/order.controller');
+const { authenticate, checkAdmin } = require('../controller/auth.controller');
+const { createOrder, getOrderList, getMyOrderList } = require('../controller/order.controller');
 
 const router = express.Router();
 
 router.post('/', authenticate, createOrder);
+router.get('/', authenticate, checkAdmin, getOrderList);
+router.get('/me', authenticate, getMyOrderList);
 
 module.exports = router;
